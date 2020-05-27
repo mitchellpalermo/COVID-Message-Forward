24b00af80abe08c232c35d018082c3a6befdcfb2
Mitch <mitchellpalermo@gmail.com>
2020-05-27 09:35:00 -0500

    used async await to clean up some promise logic.

--- index.js
+++ index.js
@@ -5,91 +5,62 @@ const twilioClient = require("twilio")(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-const googleClient = new google.auth.JWT(
-  process.env.GOOGLE_CLIENT_EMAIL,
-  null,
-  process.env.GOOGLE_PRIVATE_KEY,
-  ["https://www.googleapis.com/auth/spreadsheets"]
-);
-
-exports.forwardMessage = (req, res) => {
-  console.log("first line down");
-  googleClient.authorize(function (err, tokens) {
-    if (err) {
-      console.log(err);
-      return;
-    } else {
-      console.log("inside the else about to run the appendFunction");
-      sheetsAppend(googleClient); //if auth works, run the append function
-    }
+exports.forwardMessage = async (req, res) => {
+  console.log("First line of the func");
+  const auth = await google.auth.getClient({
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
+  const sheetsClient = google.sheets({ version: "v4", auth });
+  console.log("created the google client");
+
+  var valuesToAppend = await getTexts(twilioClient); //getting texts from twilio
+  if (valuesToAppend != null) {
+    appendValues(sheetsClient, valuesToAppend);
+  }
 };
 
-function sheetsAppend(client) {
-  console.log("made it inside the append function");
-  const sheetsClient = google.sheets({ version: "v4", auth: client });
-
+async function getTexts(twilioClient) {
   const today = new Date();
+  var valuesToAppend = [];
+  //getting texts from the last 24 hours
+  var messages = await twilioClient.messages.list({
+    dateSentAfter: new Date(
+      Date.UTC(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0)
+    ),
+    dateSentBefore: new Date(
+      Date.UTC(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate(),
+        23,
+        59,
+        0
+      )
+    ),
+    to: process.env.PHONE_NUMBER,
+    limit: 1,
+  });
 
-  twilioClient.messages
-    .list({
-      dateSentAfter: new Date(
-        Date.UTC(
-          today.getFullYear(),
-          today.getMonth(),
-          today.getDate(),
-          0,
-          0,
-          0
-        )
-      ),
-      dateSentBefore: new Date(
-        Date.UTC(
-          today.getFullYear(),
-          today.getMonth(),
-          today.getDate(),
-          23,
-          59,
-          0
-        )
-      ),
-      to: process.env.PHONE_NUMBER,
-      limit: 1,
-    })
-    .then((messages) => {
-      console.log("we made it here");
-      if (messages.length == 0) {
-        console.log(
-          "no messages for " +
-            today.getFullYear +
-            "," +
-            today.getMonth +
-            "," +
-            today.getDate +
-            "," +
-            today.getMonth +
-            "," +
-            today.getDay
-        );
-      } else {
-        var valuesToAppend = [];
-
-        messages.forEach((m) => {
-          var newRow = [m.body, m.from, m.dateSent];
-          console.log(newRow);
-          valuesToAppend.push(newRow);
-        });
-      }
-
-      const options = {
-        spreadsheetId: process.env.SPREADSHEET_ID,
-        range: "Sheet1",
-        valueInputOption: "RAW",
-        insertDataOption: "INSERT_ROWS",
-        resource: {
-          values: valuesToAppend,
-        },
-      };
-      sheetsClient.spreadsheets.values.append(options, valuesToAppend);
+  if (messages.length == 0) {
+    console.log("no messages for " + today.getDay());
+  } else {
+    messages.forEach((m) => {
+      var newRow = [m.body, m.from, m.dateSent];
+      valuesToAppend.push(newRow);
     });
+  }
+  return await valuesToAppend;
+}
+
+function appendValues(sheetsClient, valuesToAppend) {
+  const options = {
+    spreadsheetId: process.env.SPREADSHEET_ID,
+    range: "Sheet1",
+    valueInputOption: "RAW",
+    insertDataOption: "INSERT_ROWS",
+    resource: {
+      values: valuesToAppend,
+    },
+  };
+  sheetsClient.spreadsheets.values.append(options, valuesToAppend);
 }
